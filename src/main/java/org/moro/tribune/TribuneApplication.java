633fea5f604788430df21e84791549ee0005d427
Moro <648960069@qq.com>
2021-11-28 20:30:37 +0800

    init repo

---
+++ src/main/java/org/moro/tribune/TribuneApplication.java
@@ -0,0 +1,13 @@
+package org.moro.tribune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribuneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TribuneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/moro/tribune/TribuneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moro.tribune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribuneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
